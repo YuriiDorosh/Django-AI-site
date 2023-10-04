2afea6e076061488c00da084e965542c99a924eb
Yurii <yuriydorosh2005@gmail.com>
2023-10-04 22:25:52 +0300

    UPDATE: settings(postgres)

--- chattyai/settings.py
+++ chattyai/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = "chattyai.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
