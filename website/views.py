059a1d7afdefeefd91867b5371dfd440409c4d12
Yurii <yuriydorosh2005@gmail.com>
2023-10-04 21:57:14 +0300

    UPDATE: website/views(deleted useless import)

--- website/views.py
+++ website/views.py
@@ -3,7 +3,6 @@ import os
 import openai
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from dotenv import load_dotenv
